c7cbf2f4c79bdbf8516b7b76ef689309abee45a3
Diego Gonzalez <51039803+DiegoJGonzalez23@users.noreply.github.com>
2022-03-10 00:04:49 -0500

    Added more text:picture

---
+++ buttons.js
@@ -0,0 +1,22 @@
+
+let x = 1;
+function help(){
+
+    let image = document.getElementById('social');
+
+    if(x%2==1){
+
+
+       image.src = "Pictures/IMG_3163 (2).PNG";
+
+        x++;
+    }
+    else{
+        image.src = "Pictures/E3E3E3.jpg";
+        x++;
+    }
+
+
+
+
+}
